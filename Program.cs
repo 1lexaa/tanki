ac39d0477271b24048969d41a9b4ecdfd5f3e360
1lexaa <nice140@icloud.com>
2022-02-15 09:35:57 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DaySeven
+{
+    class Services
+    {
+        public static Random random = new Random();
+    }
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            var Game = new Game();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Game[i]);
+            }
+            Console.WriteLine();
+            try
+            {
+                Console.WriteLine(Game[0] ^ Game[1]);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Invalid Operation");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Argument Null");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
